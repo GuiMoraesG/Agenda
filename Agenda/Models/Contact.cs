ab1aa8d1666fb8af150a7eba437a27902cadbf9a
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2024-05-14 15:32:39 -0300

    Alterações pequenas

--- Agenda/Models/Contact.cs
+++ Agenda/Models/Contact.cs
@@ -1,10 +1,19 @@
-﻿namespace Agenda.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Agenda.Models
 {
     public class Contact
     {
         public int Id { get; set; }
+
+        [Display(Name = "Nome")]
+        [StringLength(60)]
         public string Name { get; set; }
+
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+
+        [Display(Name = "Celular")]
         public string Phone { get; set; }
         public User User { get; set; }
 
--- Agenda/Models/User.cs
+++ Agenda/Models/User.cs
@@ -1,11 +1,27 @@
-﻿namespace Agenda.Models
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+
+namespace Agenda.Models
 {
     public class User
     {
         public int Id { get; set; }
+
+        [Display(Name = "Nome")]
+        [Required(ErrorMessage = "Campo {0}, precisa ser preenchido")]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "{0} precisa estar entre {2} e {1} caracteres")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Campo {0}, precisa ser preenchido")]
+        [EmailAddress(ErrorMessage = "E-mail inválido")]
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Campo {0}, precisa ser preenchido")]
+        [StringLength(10, MinimumLength = 3, ErrorMessage = "Campo {0}, precisa ter entre {2} e {1} digitos")]
         public string Password { get; set; }
+
+        [Display(Name = "Última atualização")]
         public DateTime Cdate { get; set; }
 
         public ICollection<Contact> Contacts = new List<Contact>();
