161d7a0c30ae4b0d524688d557f4236879df442f
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2024-05-16 19:56:35 -0300

    Criando o sistema de Login

---
+++ Agenda/Components/Menu.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Agenda.Models;
+using Newtonsoft.Json;
+
+namespace Agenda.Component
+{
+    [ViewComponent(Name = "Menu")]
+    public class Menu : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            string sessionUser = HttpContext.Session.GetString("sessionUserOn");
+
+            if (string.IsNullOrEmpty(sessionUser))
+            {
+                return null;
+            }
+
+            User user = JsonConvert.DeserializeObject<User>(sessionUser);
+
+            return View(user);
+        }
+    }
+}
