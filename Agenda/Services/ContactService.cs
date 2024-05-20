dbbeec48aae9b077a87a1b0a69f0d26dc662540e
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2024-05-20 16:18:25 -0300

    Ajustes

--- Agenda/Services/ContactService.cs
+++ Agenda/Services/ContactService.cs
@@ -2,16 +2,19 @@
 using Agenda.Models;
 using Microsoft.EntityFrameworkCore;
 using Agenda.Services.Exceptions;
+using Agenda.Helper;
 
 namespace Agenda.Services
 {
     public class ContactService
     {
         private readonly AgendaContext _context;
+        private readonly Session _session;
 
-        public ContactService(AgendaContext context)
+        public ContactService(AgendaContext context, Session session)
         {
             _context = context;
+            _session = session;
         }
 
         public async Task<List<Contact>> GetContactsAsync()
@@ -26,6 +29,7 @@ namespace Agenda.Services
 
         public async Task CreateContactAsync(Contact obj)
         {
+            var userOn = _session.GetSession();
             obj.User = _context.User.First();
             await _context.AddAsync(obj);
             await _context.SaveChangesAsync();
